3b072722a5fb856770aef6205c385f5c8dd00182
gitankit7 <reachankit91@gmail.com>
2022-04-18 18:06:08 +0530

    [Ankit | Yathansh] Remove unused imports.

--- src/main/java/com/tw/bootcamp/bookshop/auth/BasicAuthConfig.java
+++ src/main/java/com/tw/bootcamp/bookshop/auth/BasicAuthConfig.java
@@ -2,18 +2,13 @@ package com.tw.bootcamp.bookshop.auth;
 
 import com.tw.bootcamp.bookshop.user.User;
 import com.tw.bootcamp.bookshop.user.UserService;
-import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-import java.net.URI;
-import java.net.URISyntaxException;
 
 @EnableWebSecurity
 public class BasicAuthConfig extends WebSecurityConfigurerAdapter {
