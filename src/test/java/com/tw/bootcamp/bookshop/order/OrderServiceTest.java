bf7235d1e1af6f714a2893d23cf238c45a0b7887
gitankit7 <reachankit91@gmail.com>
2022-04-22 10:41:19 +0530

    +Manisha | Adding new error msg for payment.

--- src/test/java/com/tw/bootcamp/bookshop/order/OrderServiceTest.java
+++ src/test/java/com/tw/bootcamp/bookshop/order/OrderServiceTest.java
@@ -31,130 +31,144 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 public class OrderServiceTest {
 
-    @InjectMocks
-    private OrderService orderService = new OrderService();
+  @MockBean BookService bookService;
+  @MockBean AddressService addressService;
+  @MockBean OrderRepository orderRepository;
+  @InjectMocks private OrderService orderService = new OrderService();
+  @Mock private RestTemplate restTemplate;
 
-    @MockBean
-    BookService bookService;
+  @Test
+  void shouldCreateOrderWhenValid()
+      throws BookNotFoundException, BookOutOfStockException, OrderNotPlacedException {
 
-    @MockBean
-    AddressService addressService;
+    CreateOrderRequest createOrderRequest = OrderTestBuilder.createOrderRequest();
+    double totalAmount = (createOrderRequest.getQuantity() * getBook().getPrice().getAmount());
+    when(bookService.findById(1L)).thenReturn(getBook());
+    when(addressService.create(createOrderRequest.getAddress(), null))
+        .thenReturn(new AddressTestBuilder().build());
+    when(orderRepository.save(any(Order.class))).thenReturn(new OrderTestBuilder().build());
 
-    @MockBean
-    OrderRepository orderRepository;
+    Order order = orderService.create(createOrderRequest);
 
-    @Mock
-    private RestTemplate restTemplate;
+    assertEquals(order.getQuantity(), createOrderRequest.getQuantity());
+    assertEquals(order.getAmount(), totalAmount);
+  }
 
+  @Test
+  void shouldDecreaseBookQuantityIfOrderIsValid()
+      throws BookNotFoundException, BookOutOfStockException, OrderNotPlacedException {
+    CreateOrderRequest createOrderRequest = OrderTestBuilder.createOrderRequest();
+    double expectedTotalAmount =
+        (createOrderRequest.getQuantity() * getBook().getPrice().getAmount());
+    int expectedCountAvailable =
+        getBook().getCountAvailable().intValue() - createOrderRequest.getQuantity();
+    Long expectedCountAvailableAsLong = (long) expectedCountAvailable;
 
-    @Test
-    void shouldCreateOrderWhenValid() throws BookNotFoundException, BookOutOfStockException, OrderNotPlacedException {
+    when(bookService.findById(1L)).thenReturn(getBook());
+    when(addressService.create(createOrderRequest.getAddress(), null))
+        .thenReturn(new AddressTestBuilder().build());
+    when(orderRepository.save(any(Order.class)))
+        .thenReturn(new OrderTestBuilder(expectedCountAvailableAsLong, 1l).build());
+    when(bookService.save(any()))
+        .thenReturn(
+            new BookTestBuilder().withBookCountAvailable(expectedCountAvailableAsLong).build());
+    Order order = orderService.create(createOrderRequest);
 
-        CreateOrderRequest createOrderRequest = OrderTestBuilder.createOrderRequest();
-        double totalAmount = (createOrderRequest.getQuantity() * getBook().getPrice().getAmount());
-        when(bookService.findById(1L)).thenReturn(getBook());
-        when(addressService.create(createOrderRequest.getAddress(), null)).thenReturn(new AddressTestBuilder().build());
-        when(orderRepository.save(any(Order.class))).thenReturn(new OrderTestBuilder().build());
+    assertEquals(createOrderRequest.getQuantity(), order.getQuantity());
+    assertEquals(expectedCountAvailable, order.getBook().getCountAvailable());
+    assertEquals(expectedTotalAmount, order.getAmount());
+  }
 
-        Order order = orderService.create(createOrderRequest);
+  private Book getBook() {
+    return new BookTestBuilder().build();
+  }
 
-        assertEquals(order.getQuantity(), createOrderRequest.getQuantity());
-        assertEquals(order.getAmount(), totalAmount);
-    }
+  @Test
+  void shouldMakePaymentWhenOrderIsValid() throws OrderException {
+    long orderId = 1L;
+    PaymentDetails paymentDetails = PaymentTestBuilder.createPaymentDetails();
 
-    @Test
-    void shouldDecreaseBookQuantityIfOrderIsValid() throws BookNotFoundException, BookOutOfStockException, OrderNotPlacedException {
-        CreateOrderRequest createOrderRequest = OrderTestBuilder.createOrderRequest();
-        double expectedTotalAmount = (createOrderRequest.getQuantity() * getBook().getPrice().getAmount());
-        int expectedCountAvailable = getBook().getCountAvailable().intValue() - createOrderRequest.getQuantity();
-        Long expectedCountAvailableAsLong = (long) expectedCountAvailable;
+    when(orderRepository.findById(any(Long.class)))
+        .thenReturn(Optional.ofNullable(new OrderTestBuilder().build()));
+    when(restTemplate.postForObject(any(String.class), any(Object.class), any()))
+        .thenReturn(new ResponseEntity<>(new Object(), HttpStatus.OK));
+    doNothing()
+        .when(orderRepository)
+        .updatePaymentStatus(any(Integer.class), any(PaymentStatus.class));
 
-        when(bookService.findById(1L)).thenReturn(getBook());
-        when(addressService.create(createOrderRequest.getAddress(), null)).thenReturn(new AddressTestBuilder().build());
-        when(orderRepository.save(any(Order.class))).thenReturn(new OrderTestBuilder(expectedCountAvailableAsLong, 1l).build());
-        when(bookService.save(any())).thenReturn(new BookTestBuilder().withBookCountAvailable(expectedCountAvailableAsLong).build());
-        Order order = orderService.create(createOrderRequest);
+    assertDoesNotThrow(() -> orderService.makePayment(Math.toIntExact(orderId), paymentDetails));
+  }
 
-        assertEquals(createOrderRequest.getQuantity(), order.getQuantity());
-        assertEquals(expectedCountAvailable, order.getBook().getCountAvailable());
-        assertEquals(expectedTotalAmount, order.getAmount());
-    }
+  @Test
+  void throwsPaymentExceptionWhenPaymentIsUnsuccessful() throws Exception {
+    long orderId = 1L;
+    PaymentDetails paymentDetails = PaymentTestBuilder.createPaymentDetails();
 
-    private Book getBook() {
-        return new BookTestBuilder().build();
-    }
+    when(orderRepository.findById(any(Long.class)))
+        .thenReturn(Optional.ofNullable(new OrderTestBuilder().build()));
+    when(restTemplate.postForObject(any(String.class), any(Object.class), any()))
+        .thenThrow(new RestClientException("Card details are invalid"));
+    doNothing()
+        .when(orderRepository)
+        .updatePaymentStatus(any(Integer.class), any(PaymentStatus.class));
 
-    @Test
-    void shouldMakePaymentWhenOrderIsValid() throws OrderException {
-        long orderId = 1L;
-        PaymentDetails paymentDetails = PaymentTestBuilder.createPaymentDetails();
+    assertThrows(PaymentException.class, () -> orderService.makePayment(orderId, paymentDetails));
+  }
 
-        when(orderRepository.findById(any(Long.class))).thenReturn(Optional.ofNullable(new OrderTestBuilder().build()));
-        when(restTemplate.postForObject(any(String.class), any(Object.class), any())).thenReturn(new ResponseEntity<>(new Object(), HttpStatus.OK));
-        doNothing().when(orderRepository).updatePaymentStatus(any(Integer.class), any(PaymentStatus.class));
+  @Test
+  void throwsOrderExceptionWhenOrderIsInvalid() throws OrderException {
+    long orderId = 1L;
+    PaymentDetails paymentDetails = PaymentTestBuilder.createPaymentDetails();
 
-        assertDoesNotThrow(() -> orderService.makePayment(Math.toIntExact(orderId), paymentDetails));
-    }
+    when(orderRepository.findById(any(Long.class))).thenReturn(Optional.empty());
 
-    @Test
-    void throwsPaymentExceptionWhenPaymentIsUnsuccessful() throws Exception {
-        long orderId = 1L;
-        PaymentDetails paymentDetails = PaymentTestBuilder.createPaymentDetails();
+    OrderException orderException =
+        assertThrows(OrderException.class, () -> orderService.makePayment(orderId, paymentDetails));
+    assertEquals("Order Not Found", orderException.getMessage());
+  }
 
-        when(orderRepository.findById(any(Long.class))).thenReturn(Optional.ofNullable(new OrderTestBuilder().build()));
-        when(restTemplate.postForObject(any(String.class), any(Object.class), any())).thenThrow(new RestClientException("Card details are invalid"));
-        doNothing().when(orderRepository).updatePaymentStatus(any(Integer.class), any(PaymentStatus.class));
+  @Test
+  void throwsPaymentAlreadyDoneExceptionWhenOrderStatusIsComplete() throws OrderException {
+    long orderId = 1L;
+    PaymentDetails paymentDetails = PaymentTestBuilder.createPaymentDetails();
+    Order order = new OrderTestBuilder().build();
+    order.setPaymentStatus(PaymentStatus.COMPLETE);
 
-        assertThrows(PaymentException.class, () -> orderService.makePayment(orderId, paymentDetails));
-    }
+    when(orderRepository.findById(any(Long.class))).thenReturn(Optional.of(order));
 
-    @Test
-    void throwsOrderExceptionWhenOrderIsInvalid() throws OrderException {
-        long orderId = 1L;
-        PaymentDetails paymentDetails = PaymentTestBuilder.createPaymentDetails();
+    PaymentAlreadyDoneException paymentAlreadyDoneException =
+        assertThrows(
+            PaymentAlreadyDoneException.class,
+            () -> orderService.makePayment(orderId, paymentDetails));
+    assertEquals(
+        "Payment is not processed because order is already paid.",
+        paymentAlreadyDoneException.getMessage());
+  }
 
-        when(orderRepository.findById(any(Long.class))).thenReturn(Optional.empty());
+  @Test
+  @DisplayName("Should throw book out of stock exception when quantiy is more than requested")
+  void shouldThrowBookOutOfStockExeption() throws BookException {
 
-        OrderException orderException = assertThrows(OrderException.class, () -> orderService.makePayment(orderId, paymentDetails));
-        assertEquals("Order Not Found",orderException.getMessage());
-    }
+    CreateOrderRequest createOrderRequest = OrderTestBuilder.createOrderRequestWithQuantity(20);
+    double totalAmount = (createOrderRequest.getQuantity() * getBook().getPrice().getAmount());
+    when(bookService.findById(1L)).thenReturn(getBook());
+    when(orderRepository.save(any(Order.class))).thenReturn(new OrderTestBuilder().build());
 
-    @Test
-    void throwsPaymentAlreadyDoneExceptionWhenOrderStatusIsComplete() throws OrderException {
-        long orderId = 1L;
-        PaymentDetails paymentDetails = PaymentTestBuilder.createPaymentDetails();
-        Order order = new OrderTestBuilder().build();
-        order.setPaymentStatus(PaymentStatus.COMPLETE);
+    assertThrows(BookOutOfStockException.class, () -> orderService.create(createOrderRequest));
+  }
 
-        when(orderRepository.findById(any(Long.class))).thenReturn(Optional.of(order));
+  @Test
+  @DisplayName("Should throw order not placed exception when the order id is null")
+  void shouldThrowOrderNotPlacedException() throws BookException, OrderException {
 
-        PaymentAlreadyDoneException paymentAlreadyDoneException =
-                assertThrows(PaymentAlreadyDoneException.class, () -> orderService.makePayment(orderId, paymentDetails));
-        assertEquals("Order is already paid",paymentAlreadyDoneException.getMessage());
-    }
+    CreateOrderRequest createOrderRequest = OrderTestBuilder.createOrderRequest();
+    double totalAmount = (createOrderRequest.getQuantity() * getBook().getPrice().getAmount());
+    when(bookService.findById(1L)).thenReturn(getBook());
+    when(addressService.create(createOrderRequest.getAddress(), null))
+        .thenReturn(new AddressTestBuilder().build());
+    when(orderRepository.save(any(Order.class)))
+        .thenReturn(new OrderTestBuilder(20l, null).build());
 
-    @Test
-    @DisplayName("Should throw book out of stock exception when quantiy is more than requested")
-    void shouldThrowBookOutOfStockExeption() throws BookException {
-
-        CreateOrderRequest createOrderRequest = OrderTestBuilder.createOrderRequestWithQuantity(20);
-        double totalAmount = (createOrderRequest.getQuantity() * getBook().getPrice().getAmount());
-        when(bookService.findById(1L)).thenReturn(getBook());
-        when(orderRepository.save(any(Order.class))).thenReturn(new OrderTestBuilder().build());
-
-        assertThrows(BookOutOfStockException.class, () -> orderService.create(createOrderRequest));
-    }
-
-    @Test
-    @DisplayName("Should throw order not placed exception when the order id is null")
-    void shouldThrowOrderNotPlacedException() throws BookException, OrderException {
-
-        CreateOrderRequest createOrderRequest = OrderTestBuilder.createOrderRequest();
-        double totalAmount = (createOrderRequest.getQuantity() * getBook().getPrice().getAmount());
-        when(bookService.findById(1L)).thenReturn(getBook());
-        when(addressService.create(createOrderRequest.getAddress(), null)).thenReturn(new AddressTestBuilder().build());
-        when(orderRepository.save(any(Order.class))).thenReturn(new OrderTestBuilder(20l, null).build());
-
-        assertThrows(OrderNotPlacedException.class, () -> orderService.create(createOrderRequest));
-    }
+    assertThrows(OrderNotPlacedException.class, () -> orderService.create(createOrderRequest));
+  }
 }
